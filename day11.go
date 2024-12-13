f161e520c9257374537bcf9a249f1da46b4386d1
iamelDuderino <andrew.j.tomko@gmail.com>
2024-12-13 14:07:56 -0500

    day 11 part 2 - big ups to hsabbas!

--- day11.go
+++ day11.go
@@ -6,8 +6,40 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
+
+// big ups to https://github.com/hsabbas
+// for the epic recursive map memoization method!
+
+// Below blink() function worked for part 1 but got sluggish around 30th iteration, crashing out around 40
+
+// func (x *aocDay11) blink() {
+// 	var insertions = make(map[int]int)
+// 	for idx, i := range x.plutonianPebbles {
+// 		switch x.consider(i) {
+// 		case d11opt1:
+// 			x.plutonianPebbles[idx] = x.opt1()
+// 		case d11opt2:
+// 			i1, i2 := x.opt2(i)
+// 			x.plutonianPebbles[idx] = i1
+// 			if len(x.plutonianPebbles)-1 == idx {
+// 				x.plutonianPebbles = append(x.plutonianPebbles, i2)
+// 			} else {
+// 				insertions[idx+1] = i2
+// 			}
+// 		case d11opt3:
+// 			x.plutonianPebbles[idx] = x.opt3(i)
+// 		}
+// 	}
+// 	var iter int
+// 	for idx, n := range insertions {
+// 		idx = idx + iter
+// 		x.plutonianPebbles = append(x.plutonianPebbles, 0)
+// 		copy(x.plutonianPebbles[idx+1:], x.plutonianPebbles[idx:])
+// 		x.plutonianPebbles[idx] = n
+// 		iter += 1
+// 	}
+// }
 
 const (
 	d11opt1 d11opt = iota
@@ -32,40 +64,48 @@ type aocDay11 struct {
 	plutonianPebbles []int
 }
 
-func (x *aocDay11) blink() {
-	var insertions = make(map[int]int)
-	for idx, i := range x.plutonianPebbles {
-		switch x.consider(i) {
-		case d11opt1:
-			x.plutonianPebbles[idx] = x.opt1()
-		case d11opt2:
-			i1, i2 := x.opt2(i)
-			x.plutonianPebbles[idx] = i1
-			if len(x.plutonianPebbles)-1 == idx {
-				x.plutonianPebbles = append(x.plutonianPebbles, i2)
-			} else {
-				insertions[idx+1] = i2
-			}
-		case d11opt3:
-			x.plutonianPebbles[idx] = x.opt3(i)
-		}
+type plutonianPebble struct {
+	num    int // original number
+	blinks int // remaining number of blinks
+} // mapped to current total
+
+func (x *aocDay11) blink(pebbles []int, blinks int) int {
+	m := make(map[plutonianPebble]int)
+	t := 0
+	for _, i := range pebbles {
+		t += x.blinkPebble(i, blinks, m)
 	}
-	var iter int
-	for idx, n := range insertions {
-		idx = idx + iter
-		x.plutonianPebbles = append(x.plutonianPebbles, 0)
-		copy(x.plutonianPebbles[idx+1:], x.plutonianPebbles[idx:])
-		x.plutonianPebbles[idx] = n
-		iter += 1
+	return t
+}
+
+func (x *aocDay11) blinkPebble(pebble int, blinks int, pebbleMap map[plutonianPebble]int) int {
+	if blinks == 0 {
+		return 1
 	}
+	prev := pebbleMap[plutonianPebble{num: pebble, blinks: blinks}]
+	if prev != 0 {
+		return prev
+	}
+	t := 0
+	switch x.consider(pebble) {
+	case d11opt1:
+		t = x.blinkPebble(x.opt1(), blinks-1, pebbleMap)
+	case d11opt2:
+		l, r := x.opt2(pebble)
+		t = x.blinkPebble(l, blinks-1, pebbleMap) + x.blinkPebble(r, blinks-1, pebbleMap)
+	case d11opt3:
+		t = x.blinkPebble(x.opt3(pebble), blinks-1, pebbleMap)
+	}
+	pebbleMap[plutonianPebble{num: pebble, blinks: blinks}] = t
+	return t
 }
 
 func (x *aocDay11) consider(n int) d11opt {
 	if n == 0 {
 		return d11opt1
 	}
-	r := []rune(fmt.Sprint(n))
-	if len(r)%2 == 0 {
+	s := fmt.Sprintf("%d", n)
+	if len(s)%2 == 0 {
 		return d11opt2
 	}
 	return d11opt3
@@ -76,22 +116,13 @@ func (x *aocDay11) opt1() int {
 }
 
 func (x *aocDay11) opt2(p int) (int, int) {
-	r := []rune(fmt.Sprint(p))
-	r1 := r[:len(r)/2]
-	r2 := r[len(r)/2:]
-	do := func(r []rune) int {
-		var s string
-		for _, i := range r {
-			s += string(i)
-		}
-		n, err := strconv.Atoi(s)
-		if err != nil {
-			fmt.Println(err)
-			return 0
-		}
-		return n
-	}
-	return do(r1), do(r2)
+	s := fmt.Sprintf("%d", p)
+	len := len(s) / 2
+	l := s[:len]
+	r := s[len:]
+	left, _ := strconv.Atoi(l)
+	right, _ := strconv.Atoi(r)
+	return left, right
 }
 
 func (x *aocDay11) opt3(p int) int {
@@ -107,13 +138,14 @@ func (x *aocDay11) readFile() {
 	s := bufio.NewScanner(f)
 	for s.Scan() {
 		s := strings.Split(s.Text(), ` `)
-		for _, i := range s {
+		x.plutonianPebbles = make([]int, len(s))
+		for idx, i := range s {
 			n, err := strconv.Atoi(i)
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
-			x.plutonianPebbles = append(x.plutonianPebbles, n)
+			x.plutonianPebbles[idx] = n
 		}
 	}
 }
@@ -125,22 +157,13 @@ func (x *aocDay11) printBanner() {
 func (x *aocDay11) part1() {
 	x.readFile()
 	numTimesToBlink := 25
-	for n := 1; n <= numTimesToBlink; n++ {
-		t := time.Now()
-		x.blink()
-		fmt.Printf(" ~ BLINK %d ~ %ss ~ %d ~\n", n, fmt.Sprintf("%.2f", time.Since(t).Seconds()), len(x.plutonianPebbles))
-	}
-	// fmt.Println(x.plutonianPebbles)
-	fmt.Println("Part 1 Solution:", len(x.plutonianPebbles))
+	total := x.blink(x.plutonianPebbles, numTimesToBlink)
+	fmt.Println("Part 1 Solution:", total)
 }
 
 func (x *aocDay11) part2() {
 	x.readFile()
 	numTimesToBlink := 75
-	for n := 1; n <= numTimesToBlink; n++ {
-		t := time.Now()
-		x.blink()
-		fmt.Printf(" ~ BLINK %d ~ %ss ~ %d ~\n", n, fmt.Sprintf("%.2f", time.Since(t).Seconds()), len(x.plutonianPebbles))
-	}
-	fmt.Println("Part 2 Solution:", len(x.plutonianPebbles))
+	total := x.blink(x.plutonianPebbles, numTimesToBlink)
+	fmt.Println("Part 2 Solution:", total)
 }
