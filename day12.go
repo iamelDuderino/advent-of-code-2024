fdb641b8ff31b0a1865f20d62f6ec4ab04c6389c
iamelDuderino <andrew.j.tomko@gmail.com>
2024-12-18 15:22:06 -0500

    Update day12.go

--- day12.go
+++ day12.go
@@ -68,11 +68,10 @@ func (x *aocDay12) get(xx, yy int) *day12plant {
 }
 
 type day12plant struct {
-	x, y                                  int
-	seed                                  string
-	isRegioned                            bool
-	regionId                              int
-	upside, downside, leftside, rightside bool
+	x, y       int
+	seed       string
+	isRegioned bool
+	regionId   int
 }
 
 type day12region struct {
@@ -84,148 +83,31 @@ type day12region struct {
 	cost                   int
 }
 
-// marks sides as traversible depending on whether or not a plant
-// can be found on that side of the plant within the same region
-func (x *day12region) markSides() {
-	for _, i := range x.plants {
-		// up
-		if x.get(i.x, i.y-1) == nil {
-			i.upside = true
-		}
-		// down
-		if x.get(i.x, i.y+1) == nil {
-			i.downside = true
-		}
-		// left
-		if x.get(i.x-1, i.y) == nil {
-			i.leftside = true
-		}
-		// right
-		if x.get(i.x+1, i.y) == nil {
-			i.rightside = true
-		}
-	}
-}
-
-func (x *day12region) checkRightAngle(p *day12plant, dir string) bool {
-	var inRegion = func(p *day12plant) bool {
-		for _, i := range x.plants {
-			if i.x == p.x && i.y == p.y {
-				return true
-			}
-		}
-		return false
-	}
+// a right angle is checked counter clockwise of the intended direction
+func (x *day12region) checkRightAngle(p *day12plant, dir string) (bool, *day12plant) {
 	switch dir {
 	case "up":
-		if p.upleft() != nil && inRegion(p.upleft()) {
-			return true
+		r := x.get(p.x-1, p.y-1)
+		if r != nil {
+			return true, r
 		}
 	case "down":
-		if p.downright() != nil && inRegion(p.downright()) {
-			return true
+		r := x.get(p.x+1, p.y+1)
+		if r != nil {
+			return true, r
 		}
 	case "right":
-		if p.upright() != nil && inRegion(p.upright()) {
-			return true
+		r := x.get(p.x+1, p.y-1)
+		if r != nil {
+			return true, r
 		}
 	case "left":
-		if p.downleft() != nil && inRegion(p.downleft()) {
-			return true
+		r := x.get(p.x-1, p.y+1)
+		if r != nil {
+			return true, r
 		}
 	}
-	return false
-}
-
-func (x *day12region) isDeadEnd(p *day12plant, dir string) bool {
-	// must have at least 3 nil sides containing no members
-	var n int
-	up := x.get(p.x, p.y-1)
-	down := x.get(p.x, p.y+1)
-	left := x.get(p.x-1, p.y)
-	right := x.get(p.x+1, p.y)
-	if up == nil {
-		n += 1
-	}
-	if down == nil {
-		n += 1
-	}
-	if left == nil {
-		n += 1
-	}
-	if right == nil {
-		n += 1
-	}
-	// must have 0 corner members in the intended direction
-	var nn int
-	upleft := x.get(p.x-1, p.y-1)
-	upright := x.get(p.x+1, p.y-1)
-	downleft := x.get(p.x-1, p.y+1)
-	downright := x.get(p.x+1, p.y+1)
-	switch dir {
-	case "up":
-		if upleft != nil {
-			nn += 1
-		}
-		if upright != nil {
-			nn += 1
-		}
-	case "down":
-		if downleft != nil {
-			nn += 1
-		}
-		if downright != nil {
-			nn += 1
-		}
-	case "left":
-		if upleft != nil {
-			nn += 1
-		}
-		if downleft != nil {
-			nn += 1
-		}
-	case "right":
-		if upright != nil {
-			nn += 1
-		}
-		if downright != nil {
-			nn += 1
-		}
-	}
-	// do a final check to determine if there are any plants to the sides of the intended
-	// direction or now in front of the recently turned tracing angles intended direction
-	var nnn int
-	switch dir {
-	case "up", "down":
-		if dir == "up" {
-			if x.get(p.x, p.y-1) != nil {
-				nnn += 1
-			}
-		}
-		if dir == "down" {
-			if x.get(p.x, p.y+1) != nil {
-				nnn += 1
-			}
-		}
-		if x.get(p.x-1, p.y) != nil || x.get(p.x+1, p.y) != nil {
-			nnn += 1
-		}
-	case "left", "right":
-		if dir == "left" {
-			if x.get(p.x-1, p.y) != nil {
-				nnn += 1
-			}
-		}
-		if dir == "right" {
-			if x.get(p.x+1, p.y+1) != nil {
-				nnn += 1
-			}
-		}
-		if x.get(p.x, p.y-1) != nil || x.get(p.x, p.y+1) != nil {
-			nnn += 1
-		}
-	}
-	return (n == 3 && nn == 0 && nnn == 0)
+	return false, nil
 }
 
 func (x *day12region) get(xx, yy int) *day12plant {
@@ -284,9 +166,9 @@ func (x *day12garden) getRegion(id int) *day12region {
 	return nil
 }
 
-// day12region.traceInsides() iterates through insides, checking
-// for internal sub-regions that must be included
-func (x *day12region) traceInsides() {
+// day12region.calculateInsides() iterates through insides, checking
+// for internal sub-regions that must be included in the total sides count
+func (x *day12region) calculateInsides() {
 	var (
 		regionsFound   = []int{}
 		inRegionsFound = func(n int) bool {
@@ -305,7 +187,7 @@ func (x *day12region) traceInsides() {
 				regionsFound = append(regionsFound, p.regionId)
 				r := day12.garden.getRegion(p.regionId)
 				if r.sides == 0 {
-					r.trace()
+					r.calculateSides()
 				}
 				x.sides += r.sides
 			}
@@ -316,7 +198,7 @@ func (x *day12region) traceInsides() {
 				regionsFound = append(regionsFound, p.regionId)
 				r := day12.garden.getRegion(p.regionId)
 				if r.sides == 0 {
-					r.trace()
+					r.calculateSides()
 				}
 				x.sides += r.sides
 			}
@@ -327,7 +209,7 @@ func (x *day12region) traceInsides() {
 				regionsFound = append(regionsFound, p.regionId)
 				r := day12.garden.getRegion(p.regionId)
 				if r.sides == 0 {
-					r.trace()
+					r.calculateSides()
 				}
 				x.sides += r.sides
 			}
@@ -338,7 +220,7 @@ func (x *day12region) traceInsides() {
 				regionsFound = append(regionsFound, p.regionId)
 				r := day12.garden.getRegion(p.regionId)
 				if r.sides == 0 {
-					r.trace()
+					r.calculateSides()
 				}
 				x.sides += r.sides
 			}
@@ -346,7 +228,7 @@ func (x *day12region) traceInsides() {
 	}
 }
 
-func (x *day12region) trace() {
+func (x *day12region) calculateSides() {
 	if x.sides > 0 {
 		return
 	}
@@ -357,220 +239,127 @@ func (x *day12region) trace() {
 		left  = "left"
 	)
 	var (
-		tracing      bool = true
 		iters        int
 		start        *day12plant = x.plants[0]
 		currentPlant *day12plant = x.plants[0]
-		dir          string      = right
-		surface      string      = up
-		inDeadend    bool
+		nextPlant    *day12plant
+		dir          string = right
+		// in order to move "forward", must not have a counter clockwise neighbor
+		hasNeighbor = func(p *day12plant, dir string) bool {
+			switch dir {
+			case right:
+				if p.y == 0 {
+					return false
+				}
+				return x.get(p.x, p.y-1) != nil
+			case down:
+				if p.x == day12.garden.width {
+					return false
+				}
+				return x.get(p.x+1, p.y) != nil
+			case left:
+				if p.y == day12.garden.length {
+					return false
+				}
+				return x.get(p.x, p.y+1) != nil
+			case up:
+				return x.get(p.x-1, p.y) != nil
+			}
+			return false
+		}
 	)
-	x.markSides()
-	for tracing {
-		// fmt.Printf("%s (%d,%d), sides: %d\n", dir, currentPlant.x, currentPlant.y, x.sides)
-		if iters >= 3 && currentPlant.x == start.x && currentPlant.y == start.y && dir == up {
-			tracing = false
+	for {
+		// fmt.Printf("(%d,%d) %s\n", currentPlant.x, currentPlant.y, dir)
+		if currentPlant.x == start.x && currentPlant.y == start.y && dir == up {
+			x.sides += 1
+			break
+		}
+		if hasNeighbor(currentPlant, dir) {
+			x.sides += 1
+			switch dir {
+			case right:
+				dir = up
+			case down:
+				dir = right
+			case left:
+				dir = down
+			case up:
+				dir = left
+			}
 		}
 		switch dir {
-		case up: // ra = upleft
-			if x.isDeadEnd(currentPlant, dir) && !inDeadend && iters > 1 && currentPlant.x != start.x && currentPlant.y != start.y {
-				switch surface {
-				case right:
-					surface = left
-				case left:
-					surface = right
-				}
-				dir = down
-				x.sides += 2
-				inDeadend = true
-			} else if currentPlant.up() != nil && currentPlant.up().seed == x.seed {
-				inDeadend = false
-				var traversible bool
-				switch surface {
-				case right:
-					traversible = currentPlant.up().rightside
-				case left:
-					traversible = currentPlant.up().leftside
-				}
-				if traversible {
-					currentPlant = currentPlant.up()
-				} else {
-					ra := x.checkRightAngle(currentPlant, dir)
-					if ra {
-						currentPlant = currentPlant.upleft()
-						surface = down
-						dir = left
-					} else {
-						surface = up
-						dir = right
-					}
-					x.sides += 1
-				}
+		case right:
+			nextPlant = x.get(currentPlant.x+1, currentPlant.y)
+			if nextPlant != nil {
+				currentPlant = nextPlant
 			} else {
-				inDeadend = false
-				ra := x.checkRightAngle(currentPlant, dir)
+				ra, p := x.checkRightAngle(currentPlant, dir)
 				if ra {
-					currentPlant = currentPlant.upleft()
-					surface = down
-					dir = left
-				} else {
-					surface = up
-					dir = right
-				}
-				x.sides += 1
-			}
-		case down: // ra = downright
-			if x.isDeadEnd(currentPlant, dir) && !inDeadend && iters > 1 && currentPlant.x != start.x && currentPlant.y != start.y {
-				switch surface {
-				case right:
-					surface = left
-				case left:
-					surface = right
-				}
-				dir = up
-				x.sides += 2
-				inDeadend = true
-			} else if currentPlant.down() != nil && currentPlant.down().seed == x.seed {
-				inDeadend = false
-				var traversible bool
-				switch surface {
-				case right:
-					traversible = currentPlant.down().rightside
-				case left:
-					traversible = currentPlant.down().leftside
-				}
-				if traversible {
-					currentPlant = currentPlant.down()
-				} else {
-					ra := x.checkRightAngle(currentPlant, dir)
-					if ra {
-						currentPlant = currentPlant.downright()
-						surface = up
-						dir = right
-					} else {
-						surface = down
-						dir = left
-					}
-					x.sides += 1
-				}
-			} else {
-				inDeadend = false
-				ra := x.checkRightAngle(currentPlant, dir)
-				if ra {
-					currentPlant = currentPlant.downright()
-					surface = up
-					dir = right
-				} else {
-					surface = down
-					dir = left
-				}
-				x.sides += 1
-			}
-		case left: // ra = downleft
-			if x.isDeadEnd(currentPlant, dir) && !inDeadend && iters > 1 && currentPlant.x != start.x && currentPlant.y != start.y {
-				switch surface {
-				case up:
-					surface = down
-				case down:
-					surface = up
-				}
-				dir = right
-				x.sides += 2
-				inDeadend = true
-			} else if currentPlant.left() != nil && currentPlant.left().seed == x.seed {
-				inDeadend = false
-				var traversible bool
-				switch surface {
-				case up:
-					traversible = currentPlant.left().upside
-				case down:
-					traversible = currentPlant.left().downside
-				}
-				if traversible {
-					currentPlant = currentPlant.left()
-				} else {
-					ra := x.checkRightAngle(currentPlant, dir)
-					if ra {
-						currentPlant = currentPlant.downleft()
-						surface = right
-						dir = down
-					} else {
-						surface = left
-						dir = up
-					}
-					x.sides += 1
-				}
-			} else {
-				inDeadend = false
-				ra := x.checkRightAngle(currentPlant, dir)
-				if ra {
-					currentPlant = currentPlant.downleft()
-					surface = left
-					dir = down
-				} else {
-					surface = left
+					currentPlant = p
 					dir = up
-				}
-				x.sides += 1
-			}
-		case right: // ra = upright
-			if x.isDeadEnd(currentPlant, dir) && !inDeadend && iters > 1 && currentPlant.x != start.x && currentPlant.y != start.y {
-				switch surface {
-				case up:
-					surface = down
-				case down:
-					surface = up
-				}
-				dir = left
-				x.sides += 2
-				inDeadend = true
-			} else if currentPlant.right() != nil && currentPlant.right().seed == x.seed {
-				inDeadend = false
-				var traversible bool
-				switch surface {
-				case up:
-					traversible = currentPlant.right().upside
-				case down:
-					traversible = currentPlant.right().downside
-				}
-				if traversible {
-					currentPlant = currentPlant.right()
+					x.sides += 1
 				} else {
-					ra := x.checkRightAngle(currentPlant, dir)
-					if ra {
-						currentPlant = currentPlant.upright()
-						surface = left
-						dir = up
-					} else {
-						surface = right
-						dir = down
-					}
+					dir = down
 					x.sides += 1
 				}
-			} else {
-				inDeadend = false
-				ra := x.checkRightAngle(currentPlant, dir)
-				if ra {
-					currentPlant = currentPlant.upright()
-					surface = left
-					dir = up
-				} else {
-					surface = right
-					dir = down
-				}
-				x.sides += 1
 			}
+		case down:
+			nextPlant = x.get(currentPlant.x, currentPlant.y+1)
+			if nextPlant != nil {
+				currentPlant = nextPlant
+			} else {
+				ra, p := x.checkRightAngle(currentPlant, dir)
+				if ra {
+					currentPlant = p
+					dir = right
+					x.sides += 1
+				} else {
+					dir = left
+					x.sides += 1
+				}
+			}
+		case left:
+			nextPlant = x.get(currentPlant.x-1, currentPlant.y)
+			if nextPlant != nil {
+				currentPlant = nextPlant
+			} else {
+				ra, p := x.checkRightAngle(currentPlant, dir)
+				if ra {
+					currentPlant = p
+					dir = down
+					x.sides += 1
+				} else {
+					dir = up
+					x.sides += 1
+				}
+			}
+		case up:
+			nextPlant = x.get(currentPlant.x, currentPlant.y-1)
+			if nextPlant != nil {
+				currentPlant = nextPlant
+			} else {
+				ra, p := x.checkRightAngle(currentPlant, dir)
+				if ra {
+					currentPlant = p
+					dir = left
+					x.sides += 1
+				} else {
+					dir = right
+					x.sides += 1
+				}
+			}
+
 		}
-		iters += 1
 		x.edges = append(x.edges, currentPlant)
-		if iters > 200 {
+		iters += 1
+		if iters > 500 {
 			x.draw()
 			msg := fmt.Sprintf("Region %d is looping @ (%d,%d)", x.id, currentPlant.x, currentPlant.y)
 			panic(msg)
 		}
 	}
 	x.fillInsides()
-	x.traceInsides()
+	x.calculateInsides()
 }
 
 func (x *day12region) calculateArea() {
@@ -678,34 +467,6 @@ func (x *day12plant) right() *day12plant {
 	return day12.get(x.x+1, x.y)
 }
 
-func (x *day12plant) upleft() *day12plant {
-	if day12.get(x.x-1, x.y-1) == nil {
-		return nil
-	}
-	return day12.get(x.x-1, x.y-1)
-}
-
-func (x *day12plant) upright() *day12plant {
-	if day12.get(x.x+1, x.y-1) == nil {
-		return nil
-	}
-	return day12.get(x.x+1, x.y-1)
-}
-
-func (x *day12plant) downleft() *day12plant {
-	if day12.get(x.x-1, x.y+1) == nil {
-		return nil
-	}
-	return day12.get(x.x-1, x.y+1)
-}
-
-func (x *day12plant) downright() *day12plant {
-	if day12.get(x.x+1, x.y+1) == nil {
-		return nil
-	}
-	return day12.get(x.x+1, x.y+1)
-}
-
 // fenceCost = regionCostA + regionCostB + etc
 //
 //	part1: regionCost = perimeter * area
@@ -761,13 +522,14 @@ func (x *aocDay12) part2() {
 	for n, i := range day12.garden.regions {
 		fmt.Printf("Calculating Region %d/%d\n", n+1, len(day12.garden.regions))
 		i.calculateArea()
-		i.trace()
+		i.calculateSides()
 		i.calculateCost("sides")
 		//
-		// i.draw()
-		// fmt.Println("Area:", i.area)
-		// fmt.Println("Sides:", i.sides)
-		// fmt.Println("Cost:", i.cost)
+		i.draw()
+		fmt.Println("Area:", i.area)
+		fmt.Println("Sides:", i.sides)
+		fmt.Println("Cost:", i.cost)
+		// break
 	}
 	fmt.Println("Part 2 Solution:", day12.garden.calculateCost())
 }
